4aae70f208d99193a401a2781e08c82b7fb17998
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2023-09-06 15:35:43 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -7,13 +7,13 @@ from pymongo.errors import DuplicateKeyError
 from umongo import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
-from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, MAX_RIST_BTNS
+from info import DATABASE_URL, DATABASE_NAME, COLLECTION_NAME, MAX_RIST_BTNS
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-client = AsyncIOMotorClient(DATABASE_URI)
+client = AsyncIOMotorClient(DATABASE_URL)
 db = client[DATABASE_NAME]
 instance = Instance.from_db(db)
 

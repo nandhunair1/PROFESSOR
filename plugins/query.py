c475fb5821242d701b0266776089735bbab0219c
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2023-09-06 17:07:44 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -429,12 +429,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "start":                        
         buttons = [[
-            InlineKeyboardButton("➕️Add Me To Your Chats ➕️", url="https://t.me/tvseriezzz_bot?startgroup=true")
+            InlineKeyboardButton("➕️Add Me To Your Chats ➕️", url="https://t.me/tvseriezzzbot?startgroup=true")
             ],[
-            InlineKeyboardButton("♻️ ⒼⓇⓄⓊⓅ ♻️", url="https://t.me/tvseriezzz_groupz"),
+            InlineKeyboardButton("♻️ ⒼⓇⓄⓊⓅ ♻️", url="https://t.me/+tvIQAggZ4hE0MDJl"),
             InlineKeyboardButton('⭕️𝚄𝙿𝙳𝙰𝚃𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻⭕️', url='https://t.me/tvseriezzz_updates')
             ],[
-            InlineKeyboardButton("♻️ ⒼⓇⓄⓊⓅ 2 ♻️", url="https://t.me/MrCVENOM_chatz"),
+            InlineKeyboardButton("♻️ ⒼⓇⓄⓊⓅ 2 ♻️", url="https://t.me/+n_rSdmDYfORlNzc1"),
             InlineKeyboardButton("🔥 Dev 🔥", callback_data='source')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),

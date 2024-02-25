9e61dd58395e0da475eb3fcf55977628bf7a398c
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2024-02-25 13:30:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -57,16 +57,16 @@ async def pm_next_page(bot, query):
     if n_offset == 0:
         btn.append(
             [InlineKeyboardButton("«««Back", callback_data=f"pmnext_{req}_{key}_{off_set}"),
-             InlineKeyboardButton(f"⚜ ᴩᴀɢᴇꜱ {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)} ⚜", callback_data="pages")]                                  
+             InlineKeyboardButton(f"⚜ ᴩᴀɢᴇꜱ {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages")]                                  
         )
     elif off_set is None:
         btn.append(
-            [InlineKeyboardButton(f"⚜ {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)} ⚜", callback_data="pages"),
+            [InlineKeyboardButton(f"⚜ {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
              InlineKeyboardButton("𝙽𝚎𝚡𝚝»»»", callback_data=f"pmnext_{req}_{key}_{n_offset}")])
     else:
         btn.append([
             InlineKeyboardButton("«««Back", callback_data=f"pmnext_{req}_{key}_{off_set}"),
-            InlineKeyboardButton(f"⚜ {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)} ⚜", callback_data="pages"),
+            InlineKeyboardButton(f"⚜ {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
             InlineKeyboardButton("𝙽𝚎𝚡𝚝»»»", callback_data=f"pmnext_{req}_{key}_{n_offset}")
         ])
     try:
@@ -130,7 +130,7 @@ async def pm_AutoFilter(client, msg, pmspoll=False):
         temp.PM_BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
-            [InlineKeyboardButton(text=f"⚜ ᴩᴀɢᴇꜱ 1/{math.ceil(int(total_results) / 6)} ⚜", callback_data="pages"),
+            [InlineKeyboardButton(text=f"⚜ ᴩᴀɢᴇꜱ 1/{math.ceil(int(total_results) / 6)}", callback_data="pages"),
             InlineKeyboardButton(text="𝙽𝚎𝚡𝚝»»»", callback_data=f"pmnext_{req}_{key}_{offset}")]
         )
     else:

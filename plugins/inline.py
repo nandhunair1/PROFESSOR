10f8e9a32b73de02e2d748341744f95266ef43cb
Venom <67759177+nandhuachu@users.noreply.github.com>
2024-02-25 14:54:17 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -96,8 +96,12 @@ async def answer(bot, query):
 
 
 def get_reply_markup(query):
-    buttons = [[InlineKeyboardButton('⟳ ꜱᴇᴀʀᴄʜ ᴀɢᴀɪɴ', switch_inline_query_current_chat=query)]]
-    return InlineKeyboardMarkup(buttons)
+    buttons = [[
+        InlineKeyboardButton("♻️ ⒼⓇⓄⓊⓅ ♻️", url="https://t.me/tvseriezzz_groupz"),
+        InlineKeyboardButton("⭕️ 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ⭕️", url="https://t.me/tvseriezzz_updates")
+        ],[
+        InlineKeyboardButton('⟳ ꜱᴇᴀʀᴄʜ ᴀɢᴀɪɴ', switch_inline_query_current_chat=query)]]
+        return InlineKeyboardMarkup(buttons)
 
 
 

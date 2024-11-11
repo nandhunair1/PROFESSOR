8c9537fe3a6dd2cbbebcea9c50eee4ae69707cd7
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2024-11-11 07:06:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,43 @@
-import os, math, logging, datetime, pytz, logging.config
+import os, math, logging, datetime, pytz
+import logging.config
 
-from aiohttp import web
-from pyrogram import Client, types
+from pyrogram.errors import BadRequest, Unauthorized
+from pyrogram import Client
+from pyrogram import types
+
+from database.ia_filterdb import Media
 from database.users_chats_db import db
-from database.ia_filterdb import  Media
-from typing import Union, Optional, AsyncGenerator
+from info import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL, UPTIME, WEBHOOK, LOG_MSG
 from utils import temp, __repo__, __license__, __copyright__, __version__
-from info import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL, UPTIME, WEB_SUPPORT, LOG_MSG
+from typing import Union, Optional, AsyncGenerator
+
+from plugins import web_server 
+from aiohttp import web
 
 # Get logging configurations
 logging.config.fileConfig("logging.conf")
-logging.getLogger(__name__).setLevel(logging.INFO)
+logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("cinemagoer").setLevel(logging.ERROR)
+logger = logging.getLogger(__name__)
 
 
 class Bot(Client):
+
     def __init__(self):
         super().__init__(
             name="Professor-Bot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            plugins=dict(root="plugins")
+            workers=200,
+            plugins={"root": "plugins"},
+            sleep_threshold=10,
         )
 
     async def start(self):
         b_users, b_chats = await db.get_banned()
         temp.BANNED_USERS = b_users
         temp.BANNED_CHATS = b_chats        
-        
         await super().start()
         await Media.ensure_indexes()
         me = await self.get_me()
@@ -43,20 +52,19 @@ class Bot(Client):
         curr = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
         date = curr.strftime('%d %B, %Y')
         tame = curr.strftime('%I:%M:%S %p')
-        logging.info(LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__))
-        
+        logger.info(LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__))
         try: await self.send_message(LOG_CHANNEL, text=LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__), disable_web_page_preview=True)   
-        except Exception as e: logging.warning(f"Bot Isn't Able To Send Message To LOG_CHANNEL \n{e}")
-        
-        if bool(WEB_SUPPORT) is True:
-            app = web.AppRunner(web.Application(client_max_size=30000000))
+        except Exception as e: logger.warning(f"Bot Isn't Able To Send Message To LOG_CHANNEL \n{e}")
+        if WEBHOOK is True:
+            app = web.AppRunner(await web_server())
             await app.setup()
             await web.TCPSite(app, "0.0.0.0", 8080).start()
-            logging.info("Web Response Is Running......🕸️")
+            logger.info("Web Response Is Running......🕸️")
             
     async def stop(self, *args):
         await super().stop()
-        logging.info(f"Bot Is Restarting ⟳...")
+        me = await self.get_me()
+        logger.info(f"{me.first_name} is_...  ♻️Restarting...")
 
     async def iter_messages(self, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:                       
         current = offset
@@ -72,6 +80,7 @@ class Bot(Client):
 
         
 Bot().run()
+
 
 
 
